2c8bc842a79778d6ee17dce1796da0c9794832e4
DuonggPh <104338962+DuonggPh@users.noreply.github.com>
2024-01-02 17:07:28 +0700

    add bomb controller and explosion controller
    bomb controller and explosion controller

--- Assets/Scripts/Bomb_Controller.cs
+++ Assets/Scripts/Bomb_Controller.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Bomb_Controller : MonoBehaviour
 {
@@ -13,8 +14,13 @@ public class Bomb_Controller : MonoBehaviour
 
     [Header("Explosion")]
     public Explosion_Controller explosionPrefab;
+    public LayerMask explosionLayerMask;
     public float explosionDuration = 1f;
-    public float explosionRadius = 1;
+    public int explosionRadius = 1;
+
+    [Header("Destructible")]
+    public Tilemap destructibleTilemaps;
+    public Destructible DestructiblePrefab;
     private void OnEnable()
     {
         bombRemain = bombAmount;
@@ -31,12 +37,56 @@ public class Bomb_Controller : MonoBehaviour
         Vector2 position = transform.position;
         position.x = Mathf.Round(position.x);
         position.y = Mathf.Round(position.y);
-
         GameObject bomb = Instantiate(bombPrefabs, position, Quaternion.identity);
         bombRemain--;
 
         yield return new WaitForSeconds(bombFuseTime);
+        position = bomb.transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+        Explosion_Controller explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(explosion.start);
+        explosion.DestroyAfter(explosionDuration);
+
+        Explode(position, Vector2.up, explosionRadius);
+        Explode(position, Vector2.down, explosionRadius);
+        Explode(position, Vector2.left, explosionRadius);
+        Explode(position, Vector2.right, explosionRadius);
+
         Destroy( bomb );
         bombRemain++;
     }
+    private void Explode(Vector2 position, Vector2 direction, int length)
+    {
+        if (length <= 0)
+        {
+            DestroyDestructible(position);
+            return;
+        }
+        position += direction;
+        if(Physics2D.OverlapBox(position, Vector2.one/2f, 0f, explosionLayerMask))
+        {
+            return;
+        }
+
+        Explosion_Controller explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(length > 1 ? explosion.mid : explosion.end);
+        explosion.SetDirection(direction);
+        explosion.DestroyAfter(explosionDuration);
+        Explode(position, direction, length - 1);
+
+    }
+
+    private void DestroyDestructible(Vector2 position)
+    {
+        Vector3Int cell = destructibleTilemaps.WorldToCell(position);
+        TileBase tile = destructibleTilemaps.GetTile(cell);
+        if (tile != null)
+        {
+            Instantiate(DestructiblePrefab, position, Quaternion.identity);
+            destructibleTilemaps.SetTile(cell, null);
+        }
+    }
 }
+
+
--- Assets/Scripts/Explosion_Controller.cs
+++ Assets/Scripts/Explosion_Controller.cs
@@ -16,7 +16,14 @@ public class Explosion_Controller : MonoBehaviour
 
     public void SetDirection(Vector2 direction)
     {
-        float angle = Mathf.Atan2(direction.x, direction.y);
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        //float angle = Mathf.Atan2(direction.x, direction.y);
+        //transform.rotation = Quaternion.AngleAxis(angle * 8, Vector3.forward);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+
+    public void DestroyAfter(float seconds)
+    {
+        Destroy(gameObject, seconds);
     }
 }
